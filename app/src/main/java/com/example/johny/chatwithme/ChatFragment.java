01cb8f77abeef64c3f5770b1db50c8b5dde879d4
nikolabastovanovic <nikola.bastovanovic@gmail.com>
2019-06-25 19:32:23 +0200

    Chat - send message

--- app/src/main/java/com/example/johny/chatwithme/ChatFragment.java
+++ app/src/main/java/com/example/johny/chatwithme/ChatFragment.java
@@ -1,11 +1,28 @@
 package com.example.johny.chatwithme;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 /**
@@ -13,6 +30,11 @@ import android.view.ViewGroup;
  */
 public class ChatFragment extends Fragment {
 
+    private View privateChatsView;
+    private RecyclerView chatList;
+    private DatabaseReference chatsRef, usersRef;
+    private FirebaseAuth mAuth;
+    private String currentUserID;
 
     public ChatFragment() {
         // Required empty public constructor
@@ -23,7 +45,99 @@ public class ChatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        privateChatsView = inflater.inflate(R.layout.fragment_chat, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        chatsRef = FirebaseDatabase.getInstance().getReference().child("Contacts").child(currentUserID);
+        usersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        chatList = (RecyclerView) privateChatsView.findViewById(R.id.chat_list);
+        chatList.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        return privateChatsView;
     }
 
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Contacts> options = new FirebaseRecyclerOptions.Builder<Contacts>()
+                .setQuery(chatsRef, Contacts.class)
+                .build();
+
+        FirebaseRecyclerAdapter<Contacts, ChatsViewHolder> adapter = new FirebaseRecyclerAdapter<Contacts, ChatsViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull final ChatsViewHolder holder, int position, @NonNull Contacts model)
+            {
+                final String usersId = getRef(position).getKey();
+                final String[] retreiveImage = {"default_image"};
+
+                usersRef.child(usersId).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot)
+                    {
+                        if (dataSnapshot.exists())
+                        {
+                            if (dataSnapshot.hasChild("image"))
+                            {
+                                retreiveImage[0] = dataSnapshot.child("image").getValue().toString();
+                                Picasso.get().load(retreiveImage[0]).into(holder.profileImage);
+                            }
+                            final String retreiveUserName = dataSnapshot.child("name").getValue().toString();
+                            final String retreiveUserStatus = dataSnapshot.child("status").getValue().toString();
+
+                            holder.userName.setText(retreiveUserName);
+                            holder.userStatus.setText("Last seen " + "\n" + "Date " + " Time");
+
+                            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v)
+                                {
+                                    Intent chatIntent = new Intent(getContext(), ChatActivity.class);
+                                    chatIntent.putExtra("visit_user_id", usersId);
+                                    chatIntent.putExtra("visit_user_name", retreiveUserName);
+                                    chatIntent.putExtra("visit_image", retreiveImage[0]);
+                                    startActivity(chatIntent);
+                                }
+                            });
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public ChatsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+            {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.users_display_layout, parent, false);
+                return new ChatsViewHolder(view);
+            }
+        };
+
+        chatList.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+    public static class ChatsViewHolder extends RecyclerView.ViewHolder
+    {
+        //include layout
+        CircleImageView profileImage;
+        TextView userName, userStatus;
+
+        public ChatsViewHolder(View itemView)
+        {
+            super(itemView);
+
+            profileImage = itemView.findViewById(R.id.users_profile_image);
+            userName = itemView.findViewById(R.id.user_profile_name);
+            userStatus = itemView.findViewById(R.id.user_status);
+        }
+    }
 }
